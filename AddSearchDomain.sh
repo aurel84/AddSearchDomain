44f334164286184489de4750b8040c4ce88974b0
aurel84 <johnhawkins3d@gmail.com>
2020-02-10 11:30:34 -0800

    Update AddSearchDomain.sh

--- AddSearchDomain.sh
+++ AddSearchDomain.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# This script allows you to automatically configure search domains for any network
-# service located in Network Prferences.  You can configure one, or multiple
-# search domains in the searchDomain array.  The for loop will then cycle through
-# the network services that the MacBook has perviously connected to and add these
-# domains to the Search Domain filed in Network Services > Advanced... > Search Domain.
-# Script was tested and has worked locally, as well as with AirWatch MDM for Macs.
-
-# By John Hawkins | johnhawkins3d@gmail.com
-
 ### array to add your own search domain ###
 searchDomains=(your.domain.1 your.domain.2 your.domain.3) # replace 'your.domain.1... 2... 3' with your own domain.  You can have fewer or more as needed
 
