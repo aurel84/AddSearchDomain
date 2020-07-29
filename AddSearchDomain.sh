f4edc67b32f4edbdaccb1da65a8db172cf14d091
aurel84 <johnhawkins3d@gmail.com>
2020-07-29 08:07:36 -0700

    Update AddSearchDomain.sh

--- AddSearchDomain.sh
+++ AddSearchDomain.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
+# Updates the search domains and dns servers for macOS in Network Preferences > Advanced... > DNS
+# Overwrites any current search domain and ip address in this entry, so please include all possible entries
+# Applies the search domain and ip addresses to all the network services in Network Preferences
+# by John Hawkins | johnhawkins3d@gmail.com
+
 ### array to add your own search domain ###
-searchDomains=(your.domain.1 your.domain.2 your.domain.3) # replace 'your.domain.1... 2... 3' with your own domain.  You can have fewer or more as needed
+searchDomains=(your.search.domain) # domains go here (replace the temporary one), leave a space between domains
+dnsServers=(1.2.3.4) # ip addresses go here (replace the temporary one), leave a space between addresses
 
-serviceList=( $(networksetup -listallnetworkservices) ) # looks for available interfaces on MacBook (Wi-Fi, Ethernet, USB, Bluetooth, etc...)
+IFS=$'\n' # Preserves spaces for network services (e.g. Thunderbolt Bridge)
+serviceList=( $(networksetup -listallnetworkservices) ) # Looks for available interfaces on MacBook (Wi-Fi, Ethernet, USB, Bluetooth, etc...)
+unset IFS
 
-### for loop that cycles through each interface that has been seen by mac book ###
+### For loop that cycles through each interface that has been seen by mac book ###
 for interface in "${serviceList[@]}";
 do
 
-  result=$(networksetup -setsearchdomains "${interface}" "${searchDomains[@]}") # for each interface that has been detected, adds the search domain from the array in line 13
-
-  if result="An asterisk (*) denotes that a network service is disabled. is not a recognized network service."; then
-
-    return > /dev/null 2>&1
-
-  else
-
-    return "$result"
-
-  fi
+    networksetup -setsearchdomains "$interface" "${searchDomains[@]}" > /dev/null 2>&1 # For each interface that has been detected, adds the search domain from the array in line 4
+    networksetup -setdnsservers "$interface" "${dnsServers[@]}" > /dev/null 2>&1 # For each interface that has been detected, adds the ip address from the array in line 5
 
 done
-
-exit
